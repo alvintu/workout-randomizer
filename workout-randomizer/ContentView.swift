c5a5356340bd9c0645b7d3019a6e7882fc86a490
Alvin Tu <alvintu@alvins-mbp-2.mynetworksettings.com>
2022-04-19 13:10:50 -0400

    First commit

--- workout-randomizer/ContentView.swift
+++ workout-randomizer/ContentView.swift
@@ -8,24 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-	let swimWorkouts = ["100 yard freestyle", "100 yard freestyle", "100 yard freestyle swim"]
+	let swimWorkouts = ["butterfly kick", "freestyle kick", "breaststroke kick", "backstroke kick", "freestyle", "butterfly", "breaststroke ", "backstroke", "one-arm butterfly", "catch-up freestyle", "super slow freestyle - focus on form", "overkick freestyle drill", "finger drag freestyle drill", "treading 5 min", "treading 2 mins - 1 hand up", "treading 1 min - no hand", "2-2-2 butterfly drill", "Head-Lead Body Dolphin butterfly drill", "kick on back - hands to side backstroke drill", "Head Above Water breaststroke", "breaststroke arms with dolphin kicks", "double arm pull backstroke", "choice - 1 lap", "choice - 2 laps"]
 	@State var workout = ""
+	@State var array: [String] = []
+
     var body: some View {
+		Image(generateImageNameFor(workout))
+			.frame(width: .infinity, height: 300, alignment: .center)
+			.clipped()
 		Text(workout)
             .padding()
 		
 		Button {
-				workout = generateWorkout()
+			withAnimation {
+			workout = generateWorkout(workout: workout)
+			}
 
-			
 		} label: {
 			Text("generate lap")
 		}
 
     }
 	
-	func generateWorkout() -> String {
-		return swimWorkouts.randomElement() ?? ""
+	func generateImageNameFor(_ workout: String) -> String {
+		if workout.contains("freestyle") {
+			return "freestyle"
+		}
+		
+		if workout.contains("breaststroke") {
+			return "breaststroke"
+		}
+		if workout.contains("butterfly") {
+			return "butterfly"
+		}
+		if workout.contains("backstroke") {
+			return "backstroke"
+		}
+		
+		if workout.contains("treading") {
+			return "treading"
+		}
+		
+		if workout.contains("choice") {
+			return "choice"
+		}
+		return ""
+		}
+	
+	
+	func generateWorkout(workout: String) -> String {
+		array = swimWorkouts.filter { $0 != workout }
+		return array.randomElement() ?? ""
 	}
 }
 	
